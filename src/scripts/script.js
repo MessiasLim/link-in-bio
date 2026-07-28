d56c1b32eaa16ff3dc7cda263bea946550dfd923
Messias Lima <messias.limatech@gmail.com>
2026-07-27 22:16:57 -0300

    feat: salva a preferência do tema usando localStorage

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -3,9 +3,23 @@ function toggleMode(){
     html.classList.toggle('light');
 
     const img = document.querySelector("#profile img");
+    
     if (html.classList.contains('light')) {
-        img.setAttribute('src', './src/images/avatar-light.jpeg')
+        img.setAttribute('src', './src/images/avatar-light.jpeg');
+        localStorage.setItem('theme', 'light');
     } else {
-        img.setAttribute('src', './src/images/avatar.jpeg')
+        img.setAttribute('src', './src/images/avatar.jpeg');
+        localStorage.setItem('theme', 'dark');
     }
 }
+
+window.addEventListener('DOMContentLoaded', () => {
+    const savedTheme = localStorage.getItem('theme');
+    const html = document.documentElement;
+    const img = document.querySelector("#profile img");
+
+    if (savedTheme === 'light') {
+        html.classList.add('light');
+        img.setAttribute('src', './src/images/avatar-light.jpeg');
+    }
+});
